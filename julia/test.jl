5eb997aa466430fb4cfee4047386e6f2c1dd5851
ctroupin <charles.troupin@gmail.com>
2020-01-08 20:15:19 +0100

    unittest

--- julia/test.jl
+++ julia/test.jl
@@ -1,45 +1,44 @@
 using Test
-include("mergingclim.jl")
+include("MergingClim.jl")
 
 # Test the creation of list of days
 yearrange = collect(2006:2010)
 monthlist = [2,5,8,11]
-dateslist = create_date_list(yearrange, monthlist)
+dateslist = MergingClim.create_date_list(yearrange, monthlist)
 datescheck = [38762, 38851, 38943, 39035, 39127, 39216,
 39308, 39400, 39492, 39582, 39674, 39766, 39858, 39947, 40039, 40131,
 40223, 40312, 40404, 40496];
 @test datescheck == dateslist;
 
 # Test depth selection
-dmin, dmax =  get_closer_depth([-10., -5., 7., 20], 1.);
+dmin, dmax =  MergingClim.get_closer_depth([-10., -5., 7., 20], 1.);
 @test dmin == -5.
 @test dmax == 7.
 
 # Test when outside the depth interval
-dmin, dmax =  get_closer_depth([-10., -5., 7., 20], 100.);
+dmin, dmax =  MergingClim.get_closer_depth([-10., -5., 7., 20], 100.);
 @test dmin == nothing
 @test dmax == nothing
 
 # Testing the linear interp
-w1, w2 = get_depth_weights(15., 10., 30.)
+w1, w2 = MergingClim.get_depth_weights(15., 10., 30.)
 a = [[1 2]; [3 4]]
 b = [[5 3]; [3 4]]
 ab = w1 * a + w2 * b
 @test ab == [[4.0  2.75]; [3.0  4.0]]
 
 # Testing the depth calculation
-w1, w2 = get_depth_weights(-10., -20., 0.)
+w1, w2 = MergingClim.get_depth_weights(-10., -20., 0.)
 @test w1 == 0.5
 @test w2 == 0.5
-w1, w2 = get_depth_weights(-5., -20., 0.)
+w1, w2 = MergingClim.get_depth_weights(-5., -20., 0.)
 @test w1 == 0.75
 @test w2 == 0.25
-w1, w2 = get_depth_weights(100., 125., 150.)
+w1, w2 = MergingClim.get_depth_weights(100., 125., 150.)
 @test w1 == nothing
 @test w2 == nothing
 
 # Testing the vertical interpolation
-
 function make4D_test(I, J, K, L)
     field4D = zeros(I, J, K, L)
     for i = 1:I
@@ -57,13 +56,13 @@ end
 f4 = make4D_test(2, 4, 3, 2)
 depthregion = [0., 10., 20., 30.]
 depthinterp = 7.5;
-f4interp = depth_interp(depthinterp, depthregion, f4)
+f4interp = MergingClim.depth_interp(depthinterp, depthregion, f4)
 @test f4interp[:,:,1] == 1.25 * ones(2, 4)
 @test f4interp[:,:,2] == 2.5 * ones(2, 4)
 
 # Test the function to get the depth indices
-depthgrid = [0, 5.3, 10.2, 24.]
-indmin, indmax = get_depth_indices(6.6, depthgrid)
+depthgrid = [0., 5.3, 10.2, 24.]
+indmin, indmax = MergingClim.get_depth_indices(6.6, depthgrid)
 @test indmin == 2
 @test indmax == 3
 
@@ -86,9 +85,9 @@ lat = collect(22.:.25:30.);
 f2 = make2D_test(length(lon), length(lat));
 longrid = collect(9:.1:16);
 latgrid = collect(21:.2:24);
-loninterp, latinterp, finterp, indlon, indlat = interp_horiz(lon, lat, f2, longrid, latgrid);
+loninterp, latinterp, finterp, indlon, indlat = MergingClim.interp_horiz(lon, lat, f2, longrid, latgrid);
 longrid2 = collect(16.5:0.1:17)
-loninterp2, latinterp2, finterp2 = interp_horiz(lon, lat, f2, longrid2, latgrid);
+loninterp2, latinterp2, finterp2 = MergingClim.interp_horiz(lon, lat, f2, longrid2, latgrid);
 
 @test loninterp[1] == maximum((lon[1], longrid[1]))
 @test loninterp[end] == minimum((lon[end], longrid[end]))
