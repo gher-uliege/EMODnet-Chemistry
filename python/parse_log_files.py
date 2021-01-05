ef098d27b541c32add4bf92d7597fa622eb0c0bd
Troupin Charles <ctroupin@uliege.be>
2021-01-05 13:21:35 +0100

    config logging

--- python/parse_log_files.py
+++ python/parse_log_files.py
@@ -9,9 +9,21 @@ logfiledir = "/home/ctroupin/Projects/EMODnet/Chemistry4/Logs"
 IP_discard_list = ["130.186.16.21"]
 
 # Configure logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('EMODnet logs ')
 logger.setLevel(logging.INFO)
-logging.info("Starting")
+# create file handler which logs even debug messages
+fh = logging.FileHandler('EMODnet-Chemistry-download.log')
+fh.setLevel(logging.INFO)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 # Regex to get the download info
 regex = ' ([(\d\.)]+) - - \[(.*?)\] "GET (.*?)" (\d*) (\d*) "-" "(.*?)"'
