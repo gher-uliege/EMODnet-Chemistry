a6dc8e2742e5b4ccf0f56b2a73a7580627770862
Alexander Barth <barth.alexander@gmail.com>
2021-03-04 14:13:11 +0000

    add combined_correlation_len.jl

---
+++ julia/combined_correlation_len.jl
@@ -0,0 +1,107 @@
+#!/usr/bin/env julia
+
+#ENV["DISPLAY"] = ""
+using Distributed
+using Dates
+using Printf
+using Random
+using NCDatasets
+@everywhere using DIVAnd
+using StatsBase
+using FileIO
+using PyPlot
+using Glob
+using JLD2
+using DataStructures
+
+include("common.jl")
+
+sz = (length(lonr),length(latr),length(depthr))
+sz = (length(lonr),length(latr))
+@show sz
+
+domains = [
+    # Arctic 1
+    #(len = 1, lonr = (-44.25,70), latr = (56.5,83)),
+    (len = 1, lonr = (-Inf,70), latr = (56.5,83)),
+    # Baltic 0.7
+    (len = 0.7, lonr = (9.4,30.9), latr = (53,60)),
+    (len = 0.7, lonr = (14,30.9), latr = (60,65.9)),
+    # NS 0.7
+    (len = 0.7, lonr = (-5.4,13), latr = (47.9,62)),
+    # # Med 2
+    (len = 2, lonr = (-0.8,36.375), latr = (30,46.375)),
+    (len = 2, lonr = (-7,36.375), latr = (30,43)),
+    # # BS 1.5
+    (len = 1.5, lonr = (26.5,41.95), latr = (40,47.95)),
+]
+
+# Atlantic 3
+default_len = 3.
+
+
+len = fill(default_len,sz)
+
+
+for i = 1:length(domains)
+    inside = (domains[i].lonr[1] .<= lonr .<= domains[i].lonr[2]) .&
+       (domains[i].latr[1] .<= latr' .<= domains[i].latr[2])
+    len[inside] .= domains[i].len
+end
+
+
+
+hx,hy,h = DIVAnd.load_bath(bathname,bathisglobal,lonr,latr)
+
+mask,pmn = DIVAnd.domain(bathname,bathisglobal,lonr,latr)
+#len[.!mask] .= NaN
+
+slen = (100e3,100e3)
+slen = (800e3,800e3)
+
+lenf = DIVAnd.diffusion(mask,pmn,slen,len)
+
+sigmoid(x) = (tanh(x)+1)/2
+
+@show sigmoid.([-2,-1,0,1,2])
+
+redlen = -0.9 * sigmoid.((-h .+ 100)/20) .+ 1
+
+nearcoast = 1.0 * (1. .- mask)
+nearcoastf = copy(nearcoast)
+
+slen = (100e3,100e3)
+nearcoastf = @time DIVAnd.diffusion(
+    trues(sz),pmn,slen,nearcoast;
+    boundary_condition! = x -> x[.!mask] .= 1
+)
+
+
+lenf2 = (1 .- nearcoastf) .* lenf + nearcoastf .* lenf/5
+
+#@code_warntype DIVAnd.diffusion(mask,pmn,slen,len)
+lenf2[.!mask] .= NaN
+
+lenfilled = DIVAnd.ufill(lenf2,isfinite.(lenf2));
+
+ioff()
+#ion()
+clf()
+title("Correlation length")
+#pcolormesh(lonr,latr,lenf'-len')
+#pcolormesh(lonr,latr,lenf')
+#pcolormesh(lonr,latr,redlen')
+#pcolormesh(lonr,latr,nearcoastf')
+pcolormesh(lonr,latr,lenf2')
+#pcolormesh(lonr,latr,lenfilled')
+aspect_ratio = 1/cosd(mean(latr))
+gca().set_aspect(aspect_ratio)
+colorbar()
+figdir = joinpath(datadir,"Figures")
+mkpath(figdir)
+savefig(joinpath(figdir,"correlation_len_$(deltalon).png"))
+
+filename_corrlen = joinpath(datadir,"correlation_len_$(deltalon).nc")
+NCDataset(filename_corrlen,"c") do ds
+    defVar(ds,"correlation_length",lenfilled,("lon","lat"))
+end
