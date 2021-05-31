93748c6039c3eba877629eb9c1b0e4ed3cf828d4
Troupin Charles <ctroupin@uliege.be>
2021-05-31 10:46:24 +0200

    conditions

--- julia/emodnetchemistry.jl
+++ julia/emodnetchemistry.jl
@@ -14,15 +14,37 @@ function get_file_list(datadir::String, varname::String="", season::String="")::
     for (root, dirs, files) in walkdir(datadir)
         for file in files
             # List only netCDF files
-            if length(varname) > 0
-                
-            if endswith(file, ".nc") &
-                (occursin(varname, file) | occursin(varname2, file)) &
-                occursin(uppercasefirst(season), root)
-                push!(filelist, joinpath(root, file))
+            if endswith(file, ".nc")
+                # Check variable name
+                if length(varname) > 0
+                    @info("Looking for variable $(varname)")
+                    # Check for seaon
+                    if length(season) > 0
+                        @info("Searching for season $(season)")
+                        if (occursin(varname, file) | occursin(varname2, file)) & occursin(uppercasefirst(season), root)
+                            push!(filelist, joinpath(root, file))
+                        end
+                    else
+                        @info("No season selected")
+                        if (occursin(varname, file) | occursin(varname2, file))
+                            push!(filelist, joinpath(root, file))
+                        end
+                    end
+                else
+                    @info("No variable selected")
+                    if length(season) > 0
+                        @info("Searching for season $(season)")
+                        if occursin(uppercasefirst(season), root)
+                            push!(filelist, joinpath(root, file))
+                        end
+                    else
+                        @info("No season selected")
+                        push!(filelist, joinpath(root, file))
+                    end
+                end
+
             end
         end
     end
     return filelist
 end
-
