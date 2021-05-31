94b5189ae8729730b82eb761f8b91c361dabf583
Troupin Charles <ctroupin@uliege.be>
2021-05-31 10:51:12 +0200

    conditions

--- julia/emodnetchemistry.jl
+++ julia/emodnetchemistry.jl
@@ -11,34 +11,43 @@ function get_file_list(datadir::String, varname::String="", season::String="")::
     filelist = []
     # Varname with spaces instead of underscores
     varname2 = replace(varname, "_" => " ")
+
+    if length(varname) > 0
+        @info("Looking for variable $(varname)")
+    else
+        @info("No variable selected")
+    end
+
+    if length(season) > 0
+        @info("Searching for season $(season)")
+    else
+        @info("No season selected")
+    end
+
+
+
     for (root, dirs, files) in walkdir(datadir)
         for file in files
             # List only netCDF files
             if endswith(file, ".nc")
                 # Check variable name
                 if length(varname) > 0
-                    @info("Looking for variable $(varname)")
                     # Check for seaon
                     if length(season) > 0
-                        @info("Searching for season $(season)")
                         if (occursin(varname, file) | occursin(varname2, file)) & occursin(uppercasefirst(season), root)
                             push!(filelist, joinpath(root, file))
                         end
                     else
-                        @info("No season selected")
                         if (occursin(varname, file) | occursin(varname2, file))
                             push!(filelist, joinpath(root, file))
                         end
                     end
                 else
-                    @info("No variable selected")
                     if length(season) > 0
-                        @info("Searching for season $(season)")
                         if occursin(uppercasefirst(season), root)
                             push!(filelist, joinpath(root, file))
                         end
                     else
-                        @info("No season selected")
                         push!(filelist, joinpath(root, file))
                     end
                 end
