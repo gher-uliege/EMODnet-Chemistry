68409a30051b1309f13c70b4512bc34115ffe4bd
Troupin Charles <ctroupin@uliege.be>
2022-07-18 15:13:53 +0200

    add new depth

--- julia/add_new_depth_layer.jl
+++ julia/add_new_depth_layer.jl
@@ -251,18 +251,18 @@ NCDatasets.Dataset(datafile1, "r") do ds
     emptyfield4D[:,:,2:end,:] = ds["Water body dissolved oxygen concentration"][:]
     ncWater_body_dissolved_oxygen_concentration[:] = emptyfield4D
 
-    emptyfield4D[:,:,2:end,:] = ds["Water body dissolved oxygen concentration L1"][:]
+    emptyfield4D[:,:,2:end,:] = ds["Water body dissolved oxygen concentration_L1"][:]
     ncWater_body_dissolved_oxygen_concentration_L1[:] = emptyfield4D
     
-    emptyfield4D[:,:,2:end,:] = ds["Water body dissolved oxygen concentration L2"][:]
+    emptyfield4D[:,:,2:end,:] = ds["Water body dissolved oxygen concentration_L2"][:]
     ncWater_body_dissolved_oxygen_concentration_L2[:] = emptyfield4D
     
     # The 'deepest' fields don't have to be modified
     ncWater_body_dissolved_oxygen_concentration_deepest[:] = ds["Water body dissolved oxygen concentration deepest"][:]
     
-    ncWater_body_dissolved_oxygen_concentration_deepest_L1[:] = ds["Water body dissolved oxygen concentration deepest L1"][:]
+    ncWater_body_dissolved_oxygen_concentration_deepest_L1[:] = ds["Water body dissolved oxygen concentration deepest_L1"][:]
     
-    ncWater_body_dissolved_oxygen_concentration_deepest_L2[:] = ds["Water body dissolved oxygen concentration deepest L2"][:]
+    ncWater_body_dissolved_oxygen_concentration_deepest_L2[:] = ds["Water body dissolved oxygen concentration deepest_L2"][:]
     
     emptyfield4D[:,:,2:end,:] = ds["Water body dissolved oxygen concentration err"][:]
     ncWater_body_dissolved_oxygen_concentration_err[:] = emptyfield4D
