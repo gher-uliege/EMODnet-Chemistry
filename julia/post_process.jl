1e0d5d00d4c509b20b2f0bc9e18c2ca5f4d22773
Charles Troupin <charles.troupin@gmail.com>
2023-09-01 15:53:22 +0200

    post processing and XML

--- julia/post_process.jl
+++ julia/post_process.jl
@@ -1,23 +1,9 @@
 #!/usr/bin/env julia
-
-#ENV["DISPLAY"] = ""
-using Distributed
 using Dates
 using Printf
-using Random
-using Profile
 using NCDatasets
-using PhysOcean
-@everywhere using DIVAnd
-using StatsBase
-using FileIO
-#using PyPlot
+using DIVAnd
 using Glob
-using JLD2
-using DataStructures
-
-ENV["JULIA_DEBUG"] = "DIVAnd"
-ENV["JULIA_DEBUG"] = ""
 
 include("common.jl")
 year0 = yearlist[1][1]
@@ -28,23 +14,39 @@ if !isfile(cdilist)
     download("https://emodnet-chemistry.maris.nl/download/export.zip",cdilist)
 end
 
-i = 1
-for i = 1:length(varlist)
+hostname = gethostname()
+if hostname == "ogs04"
+	@info "Working in production server"
+	databasedir = "/production/apache/data/emodnet-domains/By sea regions/Black Sea"
+elseif hostname == "GHER-ULg-Laptop"
+	databasedir = "/data/EMODnet/Chemistry/prod/"
+elseif hostname == "FSC-PHYS-GHER01"
+	databasedir = "/home/ctroupin/data/EMODnet-Chemistry/emodnet-results-2023"
+else
+	@error("Unknown host")
+end
 
-    varname = varlist[i]
+resdir = joinpath(databasedir, "All_European_Seas")
+
+for varname in varlist[1:1]
+
     varname_ = replace(varname," " => "_")
+    filename_src = joinpath(resdir, "$(varname_)_monthly.nc")
+    filename = replace(filename_src, "_monthly"=>"")
 
+    if isfile(filename_src)
+        @info("OK")
+    else
+        @warn(filename_src)
+    end
 
-    filename_src = joinpath(datadir,"Case/$(varname_)-res-0.25-epsilon2-2.0-varlen1-lb5-maxit-5000-reltol-1.0e-9-bathcl-go-exclude-mL-1960-exNS2-monthly/Results/$(varname_)_monthly.nc")
-
-    resdir = dirname(filename_src)
     xmlname = joinpath(resdir,"$(varname_).xml")
+    @info(xmlname);
 
-    filename = replace(filename_src,"_monthly.nc" => ".nc")
 
     DIVAnd.derived(filename_src,varname,filename)
 
-    titlestr = "European seas - DIVAnd 4D monthly analysis of $varname $year0/$year1 v2021"
+    titlestr = "European seas - DIVAnd 4D monthly analysis of $varname $year0/$year1 v2023"
     NCDataset(filename,"a") do ds
         ds.attrib["title"] = titlestr
     end
@@ -58,12 +60,11 @@ for i = 1:length(varlist)
     absstr = """
 $varname - Monthly Climatology for the European Seas for the period $(TSbackground.yearlists[1][1])-$(TSbackground.yearlists[1][end]) on the domain from longitude $(lonr[1]) to $(lonr[end]) degrees East and latitude $(latr[1]) to $(latr[end]) degrees North.
 Data Sources: observational data from SeaDataNet/EMODnet Chemistry Data Network.
-Description of DIVA analysis: The computation was done with the DIVAnd (Data-Interpolating Variational Analysis in n dimensions), version 2.7.2, using GEBCO 30sec topography for the spatial connectivity of water masses. Horizontal correlation length and vertical correlation length vary spatially depending on the topography and domain.
+Description of DIVA analysis: The computation was done with the DIVAnd (Data-Interpolating Variational Analysis in n dimensions), version 2.7.9, using GEBCO 30sec topography for the spatial connectivity of water masses. Horizontal correlation length and vertical correlation length vary spatially depending on the topography and domain.
 Depth range: $(join(string.(depthr),", ")) m.
 Units: $(varinfo[varname]["netcdf_units"]).
 The horizontal resolution of the produced DIVAnd analysis is $(step(lonr)) degrees."""
 
-    print(absstr)
 
     cd(dirname(filename)) do
         DIVAnd.divadoxml(
