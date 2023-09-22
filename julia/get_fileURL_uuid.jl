8740c1b366ced6976cdca193bc45951805ffd729
Charles Troupin <charles.troupin@gmail.com>
2023-09-22 16:38:55 +0200

    get file list

--- julia/get_fileURL_uuid.jl
+++ julia/get_fileURL_uuid.jl
@@ -66,6 +66,25 @@ function write_url_file(urlfile::String, urllist, idlist)
 end
 
 """
+    write_url_file(urlfile, theme, subtheme, filename, layername, uuid, comments)
+    )
+
+Create the table requied by Central Portal
+
+"""
+function write_url_file(urlfile::String, theme::Array, subtheme::Array, filename::Array, layername::Array, uuid, comments="")
+  open(urlfile, "w") do df
+    write(df, "Theme,	Sub-theme, File name,	Layer Name,	Metadata record UUID,	Comments\n")
+
+    nfiles = length(theme)
+    for iii=1:nfiles
+      write(df, "$(theme[iii]), $(subtheme[iii]), $(filename[iii]), $(layername[iii]), $(uuid[iii]), \n")
+    end
+  end
+  return nothing
+end
+
+"""
     check_url(fileurl)
 
 Check if the URL create by `prepare_url` is correct
@@ -76,15 +95,60 @@ function check_url(fileurl::String)
   run(testcommand)
 end
 
+"""
+    get_layer_name(datafile)
+
+Get the layer name that will be displated in the Central Portal
+(by default: the field masked with the 0.5 relative error)
+"""
+function get_layer_name(datafile::AbstractString)
+  NCDataset(datafile, "r") do ds
+    varlist = keys(ds)
+    varindex = findfirst(occursin.("_L2", varlist))
+    layername = ds[varlist[varindex]].attrib["long_name"]
+    return layername::String
+  end
+end
+"""
+    get_theme(datafile)
+
+Get the theme and sub-theme according to Central Portal requirements
+
+# Examples
+
+```julia-repl
+julia> datafile = "/production/apache/data/emodnet-projects/v2023/Coastal_areas/Northeast_Atlantic_Ocean_-_Loire_River/Water_body_phosphate.nc"
+julia> julia> get_theme(datafile)
+("Coastal areas", "Northeast Atlantic Ocean - Loire River")
+"""
+function get_theme(datafile::AbstractString)
+  pathsplit = split(datafile, "/")
+  theme = replace(pathsplit[end-2], "_"=>" ")
+  subtheme = replace(pathsplit[end-1], "_"=> " ")
+  return theme::String
+end
+
+function get_subtheme(datafile::AbstractString)
+  pathsplit = split(datafile, "/")
+  subtheme = replace(pathsplit[end-1], "_"=> " ")
+  return subtheme::String
+end
+
 # Set the directory and the name of the file which will store the lists
+# -----------------
 
 #databasedir = "/production/apache/data/emodnet-projects/Phase-3"
-databasedir = "/production/apache/data/emodnet-projects/v2023"
+#databasedir = "/production/apache/data/emodnet-projects/v2023"
+databasedir = "/home/ctroupin/data/EMODnet-Chemistry/emodnet-results-2023"
 outputfile = "./listurl_v2023.csv"
 
 datafilelist = get_netcdf_list(databasedir)
 idlist = get_id.(datafilelist)
 urllist = prepare_url.(datafilelist)
-write_url_file(outputfile, urllist, idlist)
+theme = get_theme.(datafilelist)
+subtheme = get_subtheme.(datafilelist)
+layername = get_layer_name.(datafilelist)
+
+write_url_file(outputfile, theme, subtheme, urllist, layername, idlist)
+
 @info("Info written in file $(outputfile)")
-                                                  
