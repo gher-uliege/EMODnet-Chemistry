81c23300024b8a5e036f7ce4a4a63de8d5058de3
ctroupin <ctroupin@uliege.be>
2024-11-06 12:25:30 +0100

    netCDF conversion

--- src/julia/european-climatologies/convert_netCDF_DIVAnd.jl
+++ src/julia/european-climatologies/convert_netCDF_DIVAnd.jl
@@ -21,49 +21,66 @@ varlist = ["Water body phosphate",
            ]
 
 # Set input and output directories
-inputdir = "/home/ctroupin/data/EMODnet/Eutrophication/"
-outputdir = "/home/ctroupin/data/EMODnet/Eutrophication/Split"
-
-inputfile = joinpath(inputdir, "data_from_NS_non-nutrient_data_from_time_series.nc")
-
-# Set region name (in agreeent with the input file!)
-regionname = "NorthSea_TS2"
-
-
+inputdir = "/home/ctroupin/data/EMODnet-Chemistry/Eutrophication2024/ODVnetCDF/"
+outputdir = "/home/ctroupin/data/EMODnet-Chemistry/Eutrophication2024/netCDF"
 !isdir(outputdir) ? mkdir(outputdir) : @debug("Already created")
 
-@info("Working on region $(regionname)")
-@info("File from directory: $(dirname(inputfile))")
-if isfile(inputfile)
-   @info("Start loop on variables")
+datafilesdict = Dict(
+"data_from_BALTIC_eutrophication_profiles_20240508.nc" => "BalticSea",
+"data_from_BALTIC_eutrophication_timeseries_2024.nc" => "BalticSeaTS",
+"data_from_EEA_Eutrophication_BlackSea_profiles_2024_V1.nc" => "BlackSea",
+"data_from_Eutrophication_Arctic_profiles_2024_all.nc" => "ArcticSea",
+"data_from_Eutrophication_Atlantic_profiles_2024.nc" => "Atlantic",
+"data_from_Eutrophication_Atlantic_timeseries_2024.nc" => "AtlanticTS",
+"data_from_Eutrophication_Caribbean_profiles_2024.nc" => "Caribbean",
+"data_from_Eutrophication_Caribbean_time_series_2024.nc" => "CaribbeanTS",
+"data_from_Eutrophication_MED_profiles_6.2024.nc" => "Mediterranean",
+"data_from_North_Sea_eutrhophication_profiles_20240529.nc" => "NorthSea",
+"data_from_North_Sea_eutrhophication_timeseries_20240529.nc" => "NorthSeaTS"
+)
 
-   for varname in varlist
-      @info("Working on variable $(varname)")
 
-      vnamesuffix = replace(varname, " " => "_")
-      outputfile = joinpath(outputdir, "$(regionname)_$(vnamesuffix).nc")
+# Loop on the files
+for datafile in keys(datafilesdict)
+   @info("---------------------------")
+   @info("Working on file $(datafile)")
+   @info("---------------------------")
+   inputfile = joinpath(inputdir, datafile)
 
-      if !isfile(outputfile)
+   # Set region name (in agreeent with the input file!)
+   regionname = datafilesdict[datafile]
+   @info("Region name: $(regionname)")
 
-         try
-            @info("Reading original netCDF file")
-               @time obsval, obslon, obslat, obsdepth, obstime, obsid =
-                   NCODV.load(Float64, inputfile, varname, qv_flags = ["no_quality_control", "good_value", "probably_good_value"]);
+   if isfile(inputfile)
+      @info("Start loop on variables")
 
-            @info("Writing new netCDF file")
-            @time DIVAnd.saveobs(outputfile, varname, obsval,
-               (obslon, obslat, obsdepth, obstime), obsid)
-         catch e
-            @warn("Problem to read variable $(varname) in the netCDF file")
+      for varname in varlist
+         @info("Working on variable $(varname)")
 
+         vnameclean = replace(varname, " (DIN)"=> "", " "=>"_")
+         outputfile = joinpath(outputdir, "$(regionname)_$(vnameclean).nc")
+
+         if !isfile(outputfile)
+
+            try
+               @info("Reading original netCDF file")
+                  @time obsval, obslon, obslat, obsdepth, obstime, obsid =
+                     NCODV.load(Float64, inputfile, varname, qv_flags = ["good_value", "probably_good_value"]);
+
+               @info("Writing new netCDF file")
+               DIVAnd.saveobs(outputfile, vnameclean, obsval, (obslon, obslat, obsdepth, obstime), obsid)
+            catch e
+               @warn("Problem to read variable $(varname) in the netCDF file")
+
+            end
+
+            @info("Written new file $(outputfile)")
+         else
+            @info("File $(outputfile) already created")
          end
 
-         @info("Written new file $(outputfile)")
-      else
-         @info("File $(outputfile) already created")
       end
-
+   else
+      @warn("Input file does not exist")
    end
-else
-   @warn("Input file does not exist")
 end
