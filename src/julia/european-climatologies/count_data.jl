bed3ae5d08247af0bce2b999350b7858f2b60b8c
ctroupin <ctroupin@uliege.be>
2024-11-06 12:25:18 +0100

    count data per region

---
+++ src/julia/european-climatologies/count_data.jl
@@ -0,0 +1,32 @@
+"""
+Count the number of observations in the files
+"""
+
+using NCDatasets
+using DIVAnd
+using Glob
+
+# Set list of parameters (long names)
+varlist = ["Water body phosphate",
+           "Water body dissolved oxygen concentration",
+           "Water body silicate",
+           "Water body chlorophyll-a",
+           "Water body dissolved inorganic nitrogen",
+           "Water body ammonium"
+           ]
+
+# Set input and output directories
+
+# inputdir = "/home/ctroupin/data/EMODnet/Eutrophication/"
+inputdir = "/home/ctroupin/data/EMODnet-Chemistry/Eutrophication2024/ODVnetCDF"
+# inpudir = "/home/ctroupin/data/EMODnet-Chemistry/Eutrophication2024/netCDF/"
+
+datafilelist = glob("*.nc", inputdir)
+
+for datafile in datafilelist
+    NCDataset(datafile) do ds
+        lonvar = varbyattrib(ds, standard_name="longitude")[1]
+        nobs = length(lonvar[:])
+        @info("$(basename(datafile)) -- $(nobs)")
+    end
+end
