a74155887f65175c45cea22facb811f9ea391db0
ctroupin <ctroupin@uliege.be>
2025-10-07 17:35:05 +0200

    script to edit product attributes

---
+++ src/bash/edit_productID_2023.bash
@@ -0,0 +1,44 @@
+
+
+Oxygen      37218601-e946-42d7-bdef-f94b92be7b5b
+silicate    49128d48-69c4-47fd-a112-ef892d364f34
+nitrogen    a6ac315c-c428-409e-9e73-b69c584f0d62
+phosphate   b49ca4de-1d2f-4599-9750-e44bd0e43437
+chloro      3dd096f7-1491-4bbb-9bf5-fbe04cbaed89
+ammonium    508fecce-d7cd-4af3-a83a-638aca26d101
+
+datafile="Water_body_ammonium.nc"
+ncatted -h -a product_id,global,o,c,"508fecce-d7cd-4af3-a83a-638aca26d101" ${datafile}
+ncatted -h -a doi,global,o,c,"https://doi.org/10.13120/508fecce-d7cd-4af3-a83a-638aca26d101" ${datafile}
+ncatted -h -a data_access,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+ncatted -h -a WEB_visualisation,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+
+datafile="Water_body_chlorophyll-a.nc"
+ncatted -h -a product_id,global,o,c,"3dd096f7-1491-4bbb-9bf5-fbe04cbaed89" ${datafile}
+ncatted -h -a doi,global,o,c,"https://doi.org/10.13120/3dd096f7-1491-4bbb-9bf5-fbe04cbaed89" ${datafile}
+ncatted -h -a data_access,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+ncatted -h -a WEB_visualisation,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+
+datafile="Water_body_dissolved_inorganic_nitrogen.nc"
+ncatted -h -a product_id,global,o,c,"a6ac315c-c428-409e-9e73-b69c584f0d62" ${datafile}
+ncatted -h -a doi,global,o,c,"https://doi.org/10.13120/a6ac315c-c428-409e-9e73-b69c584f0d62" ${datafile}
+ncatted -h -a data_access,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+ncatted -h -a WEB_visualisation,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+
+datafile="Water_body_dissolved_oxygen_concentration.nc"
+ncatted -h -a product_id,global,o,c,"37218601-e946-42d7-bdef-f94b92be7b5b" ${datafile}
+ncatted -h -a doi,global,o,c,"https://doi.org/10.13120/37218601-e946-42d7-bdef-f94b92be7b5b" ${datafile}
+ncatted -h -a data_access,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+ncatted -h -a WEB_visualisation,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+
+datafile="Water_body_silicate.nc"
+ncatted -h -a product_id,global,o,c,"49128d48-69c4-47fd-a112-ef892d364f34" ${datafile}
+ncatted -h -a doi,global,o,c,"https://doi.org/10.13120/49128d48-69c4-47fd-a112-ef892d364f34" ${datafile}
+ncatted -h -a data_access,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+ncatted -h -a WEB_visualisation,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+
+datafile="Water_body_phosphate.nc"
+ncatted -h -a product_id,global,o,c,"b49ca4de-1d2f-4599-9750-e44bd0e43437" ${datafile}
+ncatted -h -a doi,global,o,c,"https://doi.org/10.13120/b49ca4de-1d2f-4599-9750-e44bd0e43437" ${datafile}
+ncatted -h -a data_access,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
+ncatted -h -a WEB_visualisation,global,o,c,"https://emodnet.ec.europa.eu/geoviewer" ${datafile}
